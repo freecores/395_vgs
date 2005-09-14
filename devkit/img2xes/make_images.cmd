8ab410dd28e6e298488809b74fdb7a21a7fcaee7
zuofu <zuofu@c3865717-beeb-49af-848a-41d6bc92be36>
2005-09-14 13:54:51 +0000

    updated vga timings spreadsheet as well as test files

---
+++ devkit/img2xes/make_images.cmd
@@ -0,0 +1,3 @@
+perl img2xes.pl -x 800 -y 600 -d 3+3+3 -pi 16 -i image.png -o image_16.xes
+perl img2xes.pl -x 800 -y 600 -d 3+2+3 -pi 8  -i image.png -o image_8.xes
+perl img2xes.pl -x 800 -y 600 -d 1+1+1 -pi 4  -i image.png -o image_4.xes
